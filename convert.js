6d26ac2bb1c8d5369f2faa0461bfce0ef37fb52d
Joshua Cullen <joshua.cullen@luxgroup.com>
2018-10-30 11:39:02 +1100

    Add location display name to the airport data

--- convert.js
+++ convert.js
@@ -3,12 +3,12 @@ var JSONStream = require('JSONStream');
 var fs = require('fs');
 var _ = require('lodash');
 
-var columns = ['id', 'name', 'city', 'country', 'iata', 'icao', 'latitude', 'longitude', 'altitude', 'timezone', 'dst', 'tz'];
+var columns = ['id', 'name', 'locationDisplayName', 'city', 'country', 'iata', 'icao', 'latitude', 'longitude', 'altitude', 'timezone', 'dst', 'tz'];
 
 var readStream = fs.createReadStream('airports.dat');
 var writeStream = fs.createWriteStream('airports.json');
 
-var transformer = csv.transform(function(data) {
+var transformer = csv.transform(function (data) {
   return _.object(columns, data);
 });
 
