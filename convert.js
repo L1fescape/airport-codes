b04b975ffc80a55c7b39d533bed196293d5607c2
Tim Rogers <me@timrogers.co.uk>
2019-04-21 23:38:21 +0100

    Fix entry for Islamabad airport

--- convert.js
+++ convert.js
@@ -12,8 +12,17 @@ var transformer = csv.transform(function(data) {
   return _.object(columns, data);
 });
 
+var isbFixer = csv.transform(function(data) {
+  if (data[5] == 'OPRN') {
+    return [data[0], 'Islamabad International Airport', data[2], data[3], 'ISB', data[5], data[6], data[7], data[8], data[9]];
+  } else {
+    return data;
+  }
+})
+
 readStream
   .pipe(csv.parse())
+  .pipe(isbFixer)
   .pipe(transformer)
   .pipe(JSONStream.stringify())
   .pipe(writeStream);
